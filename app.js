246006363ac34fc06fe4dba021417b2f80a55e18
LAWAL ABD'KEHINDE <materialkenny@gmail.com>
2020-04-27 12:00:23 +0100

    emptied the list array

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(express.static('public'))
 
 app.set('view engine', 'ejs')
 
-let newTodos = ["Cook Food","Eat Food","Start Coding"]
+let newTodos = []
 app.get('/',(req,res)=>{
 
     const today = new Date()
